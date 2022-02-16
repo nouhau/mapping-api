0c11b82aa2b69f032a6d0a57f95bcb153915c4e0
Nathally Souza <nathallyccd@gmail.com>
2022-02-15 23:23:34 -0300

    Create middleware to validate authorization and add @types

---
+++ src/middlewares/verifyAuthenticated.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+import { LoggerService } from '../common/LoggerService'
+
+const logger = new LoggerService()
+
+interface IPayload {
+    sub: string;
+}
+
+export function verifyAuthenticated (request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization
+
+  if (authToken) {
+    const [, token] = authToken.split(' ')
+
+    try {
+      const { sub } = verify(token, 'token') as IPayload
+      logger.trace(
+        'Validating token',
+        'verifyAuthenticated'
+      )
+
+      request.userId = sub
+      return next()
+    } catch (error) {
+      logger.error(
+        'Invalid token',
+        'verifyAuthenticated',
+        error
+      )
+
+      return response.status(401).json({
+        message: 'User not authorized'
+      })
+    }
+  }
+
+  return response.status(401).json({
+    error: 'User not authorized'
+  })
+}
