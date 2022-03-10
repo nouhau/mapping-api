07f7fbb3f0d1482722a1102f0829d8f7fa0842c8
Nathally Souza <72207361+nathyts@users.noreply.github.com>
2022-03-10 10:42:03 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { server } from './server'
 
 const PORT = process.env.PORT || 5000
 
-console.log('JUST ANOTHER TEST')
+console.log('JUST A TEST')
 
 server.listen(PORT, () => {
   console.log(`Server on port ${PORT} \nhttp://localhost:${PORT}`)
