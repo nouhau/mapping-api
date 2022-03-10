3797a79347eb2d8f0828085d6122363f6cddacfa
Nathally Souza <nathallyccd@gmail.com>
2022-03-09 22:26:48 -0300

    Solve URL database bug

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,6 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
   entities: [
     'build/common/entities/*.js'
   ],
